45dda15e8468c6adf8bf816cd683b1ad0dd4607d
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-05-11 14:10:31 +0530

    Update Vercel.json file

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(upload());
-app.use(express.static("public"));
+app.use(express.static("public/uploads"));
 
 // Configure CORS
 app.use(cors({
